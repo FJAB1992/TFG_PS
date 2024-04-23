43aca113ab37b6db51f1b9cbfe742d89f43c670c
FJAB1992 <fjarandabarba@gmail.com>
2024-04-23 13:40:14 +0200

    format04

--- tfg_ps_app/admin.py
+++ tfg_ps_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class JugadoresAdmin(admin.ModelAdmin):
+    list_display = ("dinero",)
+
+
+admin.site.register(models.Jugadores, JugadoresAdmin)
+
+
+class InventarioAdmin(admin.ModelAdmin):
+    list_display = ("jugador_id", "objeto_id", "cantidad")
+
+
+admin.site.register(models.Inventario, InventarioAdmin)
+
+
+class ObjetosAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion", "precio", "tipo_objeto", "imagen")
+
+
+admin.site.register(models.Objetos, ObjetosAdmin)
